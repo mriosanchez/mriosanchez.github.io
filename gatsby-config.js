7e1c61ca84c7be852cb76fefd1dd3ccc17d08d25
Mario Sanchez <mriosanchezrodriguez@gmail.com>
2025-10-04 15:44:57 +0200

    changed gastby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Mario Sánchez',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Mario Sánchez is a mechanical engineer who specializes in building and maintaining machines.',
+    siteUrl: 'https://mariosanchez.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@mariito_0',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
